bea9c8ede2e29b9caf686e92d284977b95f160a8
Rishirwt <rishabhrwt03@gmail.com>
2019-07-16 16:59:00 +0530

    First lect

---
+++ src/main/java/Sale/SaleEvent.java
@@ -0,0 +1,49 @@
+package Sale;
+
+import com.google.gson.Gson;
+
+public class SaleEvent {
+    private String id;
+    private String streetAddress;
+    private String city;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "saleEvent: "+id+", "+streetAddress+", "+city;
+    }
+
+    public static void main(String[] args) {
+        SaleEvent event=new SaleEvent();
+        event.setId("100");
+        event.setStreetAddress("123 Main St.");
+        event.setCity("London");
+        System.out.println(event.toString());
+        Gson gson =new Gson();
+        String json=gson.toJson(event);
+        System.out.println(json);
+    }
+}
